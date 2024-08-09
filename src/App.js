72f83e0558fd8a985d88036d55c70ea342333554
Centy26 <centrineatieno20@gmail.com>
2024-08-09 18:33:12 +0300

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
+
+
 
 function App() {
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      <Outlet />
     </>
   );
 };
